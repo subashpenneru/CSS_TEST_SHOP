66eaa830e737e0d80d973503ba8aba00b5aa5876
subashpenneru <subashpenneru@gmail.com>
2020-09-13 12:18:17 +0530

    pagination added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import './sass/index.scss';
-
-// import { http } from './http';
 import { ui } from './ui';
 
 document.addEventListener('DOMContentLoaded', getItems);
 
+var currentPage = 1;
+var totalItems = 0;
+var totalPages = 0;
+var cartItems = [];
+var currentItems = [];
+var per_page = 3;
+
 function getItems() {
   fetch('http://localhost:3000/items')
     .then((data) => data.json())
-    .then((res) => ui.showItems(res))
+    .then((data) => {
+      cartItems = data;
+      totalItems = data.length;
+      totalPages = Math.ceil(data.length / per_page);
+      showCartItems(cartItems);
+    })
     .catch((err) => console.log(err));
 }
+
+function showCartItems(data = []) {
+  currentItems = cartItems.slice(
+    (currentPage - 1) * per_page,
+    currentPage * per_page
+  );
+
+  ui.showItems(currentItems);
+
+  ui.showPaginatior(totalPages, currentPage, per_page);
+}
+
+const onPagination = (_page) => {
+  if (_page === 'prev') {
+    if (currentPage > 1 && currentPage <= totalPages) {
+      currentPage -= 1;
+    }
+  } else if (_page === 'next') {
+    if (currentPage < totalPages) {
+      currentPage += 1;
+    }
+  } else {
+    currentPage = _page;
+  }
+
+  showCartItems(cartItems);
+};
+
+window.onPagination = onPagination;
--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,7 @@
 class UI {
   constructor() {
     this.items = document.querySelector('.cart__items');
+    this.paginator = document.querySelector('.pagination');
   }
 
   showItems(items) {
@@ -13,7 +14,7 @@ class UI {
           <h3 class="cart__title">${el.name}</h3>
           <div class="cart__info">
             <p class="cart__info-title">${el['shop-item']}</p>
-            <p>${el.price}</p>
+            <p>$${el.price}</p>
           </div>
           <div class="cart__footer">
             <div class="cart__rating">
@@ -40,6 +41,53 @@ class UI {
     }
 
     return rating;
+  }
+
+  showPaginatior(totalPages, cur_page, per_page) {
+    let start_page = 1;
+    let last_page = 3;
+
+    if (totalPages <= last_page) {
+      last_page = totalPages;
+    }
+
+    if (cur_page > last_page) {
+      start_page = cur_page + start_page - last_page;
+      last_page = cur_page;
+    }
+
+    if (cur_page < start_page) {
+      last_page = cur_page - start_page + last_page;
+      start_page = cur_page;
+    }
+
+    let left_arr = '';
+    let right_arr = '';
+    let pagination = '';
+
+    if (cur_page > 1) {
+      left_arr = `<div class="pagination__options" onclick="onPagination('prev')">
+      <i class="fa fa-chevron-left"></i>
+    </div>`;
+    }
+
+    if (cur_page < totalPages) {
+      right_arr = `<div class="pagination__options" onclick="onPagination('next')">
+      <i class="fa fa-chevron-right"></i>
+    </div>`;
+    }
+
+    if (totalPages > 0) {
+      pagination += `<div class="pagination__items">`;
+      for (let i = start_page; i <= last_page; i++) {
+        pagination += `<span class="pagination__item ${
+          cur_page === i ? 'active' : null
+        }" onclick="onPagination(${i})">${i}</span>`;
+      }
+      pagination += `</div>`;
+    }
+
+    this.paginator.innerHTML = left_arr + pagination + right_arr;
   }
 }
 
